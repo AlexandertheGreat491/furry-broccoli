e87003146a49dd9ab93e85c014abda58315ac3ad
agvan <agvandyke25@gmail.com>
2022-05-21 15:04:17 -0500

    added comments to put user story & acceptance criteria in my own words

---
+++ script.js
@@ -0,0 +1,12 @@
+/*The purpose of this project is to create a work day scheduler
+for an employee with a busy schedule where they can add important eveants to a daily
+planner and manage their time effectively.*/
+
+//User opens the daily planner to create a schedule and the current day is displayed at the top of the calendar.
+//User is presented with time blocks for that day in standard business hours.
+//User views color-coded time-blocks to that day, indicating whether it is in the past, present, or the future.
+//User clicks on the time block and can enter an event.
+//User clicks on the save button for that timeblock, the text is save in local storage.
+//User refreshes the page and the saved event persists.
+
+
