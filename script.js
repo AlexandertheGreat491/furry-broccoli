7b7a33c225c480c7048944011b547b02de7d3e51
agvan <agvandyke25@gmail.com>
2022-05-22 18:32:31 -0500

    blank lines added for readability

--- script.js
+++ script.js
@@ -19,10 +19,12 @@ $(document).ready(function () {
         //Retrieves the nearest values of the description.
         var text = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
+        
         //.siblings() gets the siblings of the set of elements that match.
         //.parent() gets the parent of the matching element.
 
         // The text will be saved to local storage.
+
         localStorage.setItem(time, text);
     })
 
