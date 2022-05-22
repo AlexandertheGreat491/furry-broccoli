576d30e326dc06581a8e12c38f9e236188ef024b
agvan <agvandyke25@gmail.com>
2022-05-22 15:06:03 -0500

    updated local storage

--- script.js
+++ script.js
@@ -60,15 +60,17 @@ function daySchedule() {
 
 // The item will be retrieved from local storage if it exists.
 
-$("#nine .description").val(localStorage.getItem("nine"));
-$("#ten .description").val(localStorage.getItem("ten"));
-$("#eleven .description").val(localStorage.getItem("eleven"));
-$("#noon .description").val(localStorage.getItem("noon"));
-$("#one .description").val(localStorage.getItem("one"));
-$("#two .description").val(localStorage.getItem("two"));
-$("#three .description").val(localStorage.getItem("three"));
-$("#four .description").val(localStorage.getItem("four"));
-$("#five .description").val(localStorage.getItem("five"));
+$("#hour8 .description").val(localStorage.getItem("hour8"));
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
+
 
 daySchedule();
 
